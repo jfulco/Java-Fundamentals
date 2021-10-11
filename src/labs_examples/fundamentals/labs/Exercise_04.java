496e7e9f9fdf9005d984a701388ed1784d61bbc5
jfulco <jaquelynefulco@gmail.com>
2021-10-10 23:42:02 -0400

    Finished Exercise 04 of fundamentals labs.

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,8 @@
 package labs_examples.fundamentals.labs;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 4: My Second Program
  *
@@ -14,6 +17,31 @@ public class Exercise_04 {
 
         int i = 1;
         System.out.println("int i is: " + i);
+        System.out.println("Primitive Data Type codes declared and executed by Jackie!");
+        boolean isEmpty = false;
+        System.out.println("boolean isEmpty is: "  + isEmpty);
+
+        char a = 'A';
+        System.out.println("char a is: " + a);
+
+        byte byteVal = 23;
+        System.out.println("byte byteVal is: " + byteVal);
+
+        short shortVal = 343;
+        System.out.println("short shortVal is: " + shortVal);
+
+        int intVal = 5677756;
+        System.out.println("int intVal is: " + intVal);
+
+        long longVal = 4567854432L;
+        System.out.println("long longVal is: " + longVal);
+
+        float floatVal = 34324.2f;
+        System.out.println("float floatVal is: " + floatVal);
+
+        double doubleVal = 908788328.2123;
+        System.out.println("double doubleVal is: " + doubleVal);
+
 
         // write your code below
 
