8b44bd0bfcb1cba97ad4c3b24f2cdfa9bd3f4415
jfulco <jaquelynefulco@gmail.com>
2021-10-25 15:57:41 -0400

    Completed arrays lab, exercise 1, 2, & 3.

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,4 +1,5 @@
 package labs_examples.arrays.labs;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -11,5 +12,44 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        Scanner inputOne = new Scanner(System.in);
+        Scanner inputTwo = new Scanner(System.in);
+        Scanner inputThree = new Scanner(System.in);
+        Scanner inputFour = new Scanner(System.in);
+        Scanner inputFive = new Scanner(System.in);
+        Scanner inputSix = new Scanner(System.in);
+        Scanner inputSeven = new Scanner(System.in);
+        Scanner inputEight = new Scanner(System.in);
+        Scanner inputNine = new Scanner(System.in);
+        Scanner inputTen = new Scanner(System.in);
+
+        System.out.print("Type 10 numbers between 0 to 100, hitting 'Enter' after each number: ");
+        System.out.println();
+
+        int [] vals = new int[10];
+        vals[0] = inputOne.nextInt();
+        vals[1] = inputTwo.nextInt();
+        vals[2] = inputThree.nextInt();
+        vals[3] = inputFour.nextInt();
+        vals[4] = inputFive.nextInt();
+        vals[5] = inputSix.nextInt();
+        vals[6] = inputSeven.nextInt();
+        vals[7] = inputEight.nextInt();
+        vals[8] = inputNine.nextInt();
+        vals[9] = inputTen.nextInt();
+
+        System.out.println("Now lets find the sum of ALL the values you entered.");
+        int sum = 0;
+        for (int i = 0; i < vals.length; i++){
+            System.out.println("The number you entered at index " + i + " is " + vals[i] + ".");
+            sum += vals[i];
+        }
+        int average = sum/vals.length;
+        System.out.println("The sum of all your numbers is " + sum + ".");
+        System.out.println("The average of all your numbers is " + average + ".");
+
+
+    }
 
 }
--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,4 +1,5 @@
 package labs_examples.arrays.labs;
+import java.util.Scanner;
 
 /**
  *  More labs_examples.arrays
@@ -11,11 +12,34 @@ package labs_examples.arrays.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        // write code here
+        System.out.println("I have a 10 slot array already populated.");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number 1 - 10 to reveal where it's located (what index) in the array: ");
+        int num = input.nextInt();
 
+        if (num == array[0]) {
+            System.out.println("Your number is at index 0 in the array.");
+        } else if (num == array[1]) {
+            System.out.println("Your number is at index 1 in the array.");
+        } else if (num == array[2]) {
+            System.out.println("Your number is at index 2 in the array.");
+        } else if (num == array[3]) {
+            System.out.println("Your number is at index 3 in the array.");
+        } else if (num == array[4]) {
+            System.out.println("Your number is at index 4 in the array.");
+        } else if (num == array[5]) {
+            System.out.println("Your number is at index 5 in the array.");
+        } else if (num == array[6]) {
+            System.out.println("Your number is at index 6 in the array.");
+        } else if (num == array[7]) {
+            System.out.println("Your number is at index 7 in the array.");
+        } else if (num == array[8]) {
+            System.out.println("Your number is at index 8 in the array.");
+        } else if (num == array[9]) {
+            System.out.println("Your number is at index 9 in the array.");
+       }
 
     }
 }
